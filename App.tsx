c646c0f2893c084d2bbc17725e7b6c0b62dea1c8
LuizTimboAlcantara <joseluiztimbo@gmail.com>
2021-08-11 23:55:15 -0300

    Stack Navigation - parte 01.

--- App.tsx
+++ App.tsx
@@ -6,9 +6,7 @@ import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import { Rajdhani_500Medium, Rajdhani_700Bold } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
 
-import { Background } from "./src/components/Background";
-import { SignIn } from "./src/screens/SignIn";
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_500Medium, Rajdhani_500Medium, Rajdhani_700Bold });
@@ -18,10 +16,9 @@ export default function App() {
   }
 
   return (
-    <Background>
+    <>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-
-      <Home />
-    </Background>
+      <Routes />
+    </>
   );
 }
