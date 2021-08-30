955989e42d667d94c21afde7d875083dafce4896
Luiz Timbo <joseluiztimbo@gmail.com>
2021-08-30 16:54:23 -0300

    Aula 03 parte 02

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,12 @@
 import React, { ReactNode } from "react";
-import { View, Text } from "react-native";
-
 import { LinearGradient } from "expo-linear-gradient";
 import { BorderlessButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
-
-import { theme } from "../../global/styles/theme";
+import { Text, View } from "react-native";
 
 import { styles } from "./styles";
+import { theme } from "../../global/styles/theme";
 
 type Props = {
   title: string;
@@ -17,15 +16,21 @@ type Props = {
 export function Header({ title, action }: Props) {
   const { secondary100, secondary40, heading } = theme.colors;
 
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <LinearGradient style={styles.container} colors={[secondary100, secondary40]}>
-      <BorderlessButton>
+      <BorderlessButton onPress={handleGoBack}>
         <Feather name="arrow-left" size={24} color={heading} />
-
-        <Text style={styles.title}>{title}</Text>
-
-        {action && <View>{action}</View>}
       </BorderlessButton>
+
+      <Text style={styles.title}>{title}</Text>
+
+      {action ? <View>{action}</View> : <View style={{ width: 24 }} />}
     </LinearGradient>
   );
 }
